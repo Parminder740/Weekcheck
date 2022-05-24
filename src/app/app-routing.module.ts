ca56428c5e42e241646348060c37fc9109d6f57d
Sandeep Gautam <sandeepgautam.tp@gmail.com>
2022-05-25 02:36:24 +0530

    businessess

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 
 import { AddOrJoinBussinessComponent } from './add-or-join-bussiness/add-or-join-bussiness.component';
-=======
-import { LocationDetailsComponent } from './location-details/location-details.component';
->>>>>>> 3f1db2d21a50a3fb90be0104587eec43cf783659
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
@@ -13,11 +9,7 @@ import { SigninComponent } from './signin/signin.component';
 const routes: Routes = [
 {path:"register",component:RegisterComponent},
 {path:"",component:LoginComponent},
-<<<<<<< HEAD
 {path:"addorjoin",component:AddOrJoinBussinessComponent}
-=======
-{path:"signin",component:SigninComponent}
->>>>>>> 3f1db2d21a50a3fb90be0104587eec43cf783659
 ];
 
 @NgModule({
