b8360b4d60c14030ccad5bd91cee3cb1b502e5fe
Parminder740 <parmindersingh740@gmail.com>
2022-05-25 23:07:20 +0530

    global scss added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
-
+import { LocationDetailsComponent } from './location-details/location-details.component';
 const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-
-  { path: "addorjoin", component: AddOrJoinBussinessComponent }
+  { path: "addorjoin", component: AddOrJoinBussinessComponent },
+  { path: "location-details", component: LocationDetailsComponent }
 ];
 
 @NgModule({
