fbffd225282ed29474682a73f3beb27881fdac3e
Parminder740 <parmindersingh740@gmail.com>
2022-05-26 15:59:32 +0530

    par

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
-
 import { AddOrJoinBussinessComponent } from './add-or-join-bussiness/add-or-join-bussiness.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
-
+import { DashboardComponent } from "./dashboard/dashboard.component";
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 {path:"register",component:RegisterComponent},
-{path:"",component:LoginComponent},
-{path:"addorjoin",component:AddOrJoinBussinessComponent}
+{path:"login",component:LoginComponent},
+{path:"addorjoin",component:AddOrJoinBussinessComponent},
+{path:"dashboard",component:DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,32 +7,23 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
-<<<<<<< HEAD
 import { AddOrJoinBussinessComponent } from './add-or-join-bussiness/add-or-join-bussiness.component';
-=======
-<<<<<<< HEAD
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { SigninComponent } from './signin/signin.component';
-=======
 import { LocationDetailsComponent } from './location-details/location-details.component';
->>>>>>> 3f1db2d21a50a3fb90be0104587eec43cf783659
+import { DashboardComponent } from './dashboard/dashboard.component';
 
->>>>>>> aaba1ef5b43cfa4474cd581a7a330fe2f021d7f4
+
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     LoginComponent,
-<<<<<<< HEAD
-    AddOrJoinBussinessComponent
-=======
-<<<<<<< HEAD
-    SigninComponent
-=======
-    LocationDetailsComponent
->>>>>>> aaba1ef5b43cfa4474cd581a7a330fe2f021d7f4
->>>>>>> 3f1db2d21a50a3fb90be0104587eec43cf783659
+    AddOrJoinBussinessComponent,
+    SigninComponent,
+    LocationDetailsComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
